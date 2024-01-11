591ca97e928ab4c56748f34bf183f5cf8c5fe439
BenjaminEnsea <118808253+BenjaminEnsea@users.noreply.github.com>
2024-01-11 01:40:07 +0100

    Update main copy.cu

--- main copy.cu
+++ main copy.cu
@@ -1,11 +1,1 @@
-// ------------------------ Convolution of Matrix -------------------------------
-
-
-
-int main(void){
-
-    clock_t start_cpu, end_cpu;
-    double time_used;
-
-    
-}
+// empty 
